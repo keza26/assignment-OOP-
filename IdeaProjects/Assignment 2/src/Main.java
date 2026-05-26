f4f5e5d367509654a46e9999cabb907195fba351
k-gloire <g.umuhendak@alustudent.com>
2026-05-26 10:26:56 +0200

    This is my assignment

---
+++ IdeaProjects/Assignment 2/src/Main.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+      Scanner input = new Scanner(System.in);
+      Doctor d1 = new Doctor(3, "Dr John",45);
+      Patient p1 = new Patient(4,"Alice",22);
+
+        System.out.println("------HOSPITAL MENU-----");
+
+        System.out.println("1. Check Doctor");
+        System.out.println("2. Check Patients");
+        System.out.println("3. Add Patient");
+        System.out.println("4. Exit");
+
+        System.out.println("Choose option: ");
+
+        int choice =
+
+        }
+    }
